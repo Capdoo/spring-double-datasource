58d7f9a05b3e01b1b463ecbbf7446680863f4ce1
Capdoo <mazher_2014@hotmail.com>
2022-02-10 16:43:41 -0500

    First Commit

---
+++ src/main/java/com/datasource/app/SpringDoubleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.datasource.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDoubleDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDoubleDatasourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datasource/app/SpringDoubleDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasource.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDoubleDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
