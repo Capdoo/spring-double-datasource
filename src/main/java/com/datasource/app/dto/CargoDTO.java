999366a5b37b7217260cfa2aaa8d43ab00316e3a
Capdoo <mazher_2014@hotmail.com>
2022-02-18 12:57:12 -0500

    Modalidades

--- src/main/java/com/datasource/app/dto/CargoDTO.java
+++ src/main/java/com/datasource/app/dto/CargoDTO.java
@@ -1,32 +1,52 @@
 package com.datasource.app.dto;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
-import com.datasource.app.models.EmpleadoModel;
-import com.datasource.app.models.ModalidadModel;
-
 public class CargoDTO {
-
 
 	//private long idCargo;
 	
-
 	private String nombre;
-
 
 	private String area;
 	
+	private long idModalidad;
+
+	public CargoDTO() {
+		super();
+	}
+	
+	public CargoDTO(String nombre, String area, long idModalidad) {
+		super();
+		this.nombre = nombre;
+		this.area = area;
+		this.idModalidad = idModalidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public long getIdModalidad() {
+		return idModalidad;
+	}
+
+	public void setIdModalidad(long idModalidad) {
+		this.idModalidad = idModalidad;
+	}
 	//private EmpleadoModel empleado;
 
-	//private ModalidadModel modalidad;
-	
+	//private ModalidadModel modalidad;	
 }
 
 
